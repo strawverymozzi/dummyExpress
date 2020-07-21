f7aaa2814693f1e2d685244eb46fd296dbb9386f
strawverymozzi <jswon0712@hanmail.net>
2020-07-21 18:56:08 +0900

    123

--- service.js
+++ service.js
@@ -15,10 +15,10 @@ class Service {
     static fiddleDataSource(filePath, taskfunction) {
         let rs;
         return new Promise((res, rej) => {
-            const data = fs.readFileSync(filePath, 'utf8', function readFileCallback(err, data) {});
+            const data = fs.readFileSync(filePath, 'utf8', function readFileCallback(err, data) { });
             const jsonDB = JSON.parse(data); //now it an object
             rs = taskfunction(jsonDB); //add some data
-            fs.writeFileSync(filePath, JSON.stringify(jsonDB, null, "\t"), 'utf8', function () {}); // write it back 
+            fs.writeFileSync(filePath, JSON.stringify(jsonDB, null, "\t"), 'utf8', function () { }); // write it back 
             res(rs);
         })
     }
@@ -49,7 +49,7 @@ class Service {
                 return {
                     "success": true,
                     "code": 0,
-                    "msg": target == "listRcv"? "마스터 INSERT 성공하였습니다.":"디테일 INSERT 성공하였습니다.",
+                    "msg": target == "listRcv" ? "마스터 INSERT 성공하였습니다." : "디테일 INSERT 성공하였습니다.",
                     "list": [data]
                 };
             }
@@ -63,7 +63,7 @@ class Service {
                         return {
                             "success": true,
                             "code": 0,
-                            "msg": target == "listRcv"? "마스터 UPDATE 성공하였습니다.":"디테일 UPDATE 성공하였습니다.",
+                            "msg": target == "listRcv" ? "마스터 UPDATE 성공하였습니다." : "디테일 UPDATE 성공하였습니다.",
                             "list": [data]
                         };
                     }
@@ -137,49 +137,49 @@ class Service {
             "msg": "성공하였습니다.",
             "transationTime": "2020-05-17T20:12:56.9294169",
             "list": [{
-                    "uid": 10000,
+                "uid": 10000,
+                "tenant": "1000",
+                "title": "MENU1",
+                "icon": "shopping-cart-outline",
+                "menuPath": "10000",
+                "appUid": null,
+                "url": null,
+                "link": null,
+                "windowName": null,
+                "children": [{
+                    "uid": 10002,
                     "tenant": "1000",
-                    "title": "MENU1",
+                    "title": "입고예정",
                     "icon": "shopping-cart-outline",
-                    "menuPath": "10000",
-                    "appUid": null,
-                    "url": null,
-                    "link": null,
-                    "windowName": null,
-                    "children": [{
-                        "uid": 10002,
-                        "tenant": "1000",
-                        "title": "입고예정",
-                        "icon": "shopping-cart-outline",
-                        "menuPath": "10000|10002",
-                        "appUid": 90003,
-                        "url": "http://localhost:3001/api/global/sampleComp",
-                        "link": "/adminPages/CM/application3",
-                        "windowName": "rcv",
-                        "children": [],
-                        "insFlg": "I3bf0faZOdYKsS5EhYGUStkWhWiBV8gBC5p1FG8xz5U=",
-                        "updFlg": "LkhF1x2Eva7i9SYNof0fetkWhWiBV8gBC5p1FG8xz5U=",
-                        "delFlg": ""
-                    }],
-                    "insFlg": null,
-                    "updFlg": null,
-                    "delFlg": null
-                },
-                {
-                    "uid": 10001,
-                    "tenant": "1000",
-                    "title": "MENU2",
-                    "icon": "shopping-cart-outline",
-                    "menuPath": "10001",
-                    "appUid": 90001,
-                    "url": "/CM/test1",
-                    "link": "/adminPages/AdminModule1Module/test1",
-                    "windowName": null,
+                    "menuPath": "10000|10002",
+                    "appUid": 90003,
+                    "url": "http://localhost:3001/api/global/sampleComp",
+                    "link": "/adminPages/CM/application3",
+                    "windowName": "rcv",
                     "children": [],
-                    "insFlg": "02W4Lc/aYBMcIVioXkTauRnoFbEk1cqa4/mCe/vGkl8=",
-                    "updFlg": "hmxY0gurkbZKWgPc6/F2T2S6MlbNlnG3Vit+rhh3swk=",
-                    "delFlg": "POa4BMjxP1K9uqzyh5Sfbw40bL2g90IdSu66IniFxK4="
-                }
+                    "insFlg": "I3bf0faZOdYKsS5EhYGUStkWhWiBV8gBC5p1FG8xz5U=",
+                    "updFlg": "LkhF1x2Eva7i9SYNof0fetkWhWiBV8gBC5p1FG8xz5U=",
+                    "delFlg": ""
+                }],
+                "insFlg": null,
+                "updFlg": null,
+                "delFlg": null
+            },
+            {
+                "uid": 10001,
+                "tenant": "1000",
+                "title": "MENU2",
+                "icon": "shopping-cart-outline",
+                "menuPath": "10001",
+                "appUid": 90001,
+                "url": "/CM/test1",
+                "link": "/adminPages/AdminModule1Module/test1",
+                "windowName": null,
+                "children": [],
+                "insFlg": "02W4Lc/aYBMcIVioXkTauRnoFbEk1cqa4/mCe/vGkl8=",
+                "updFlg": "hmxY0gurkbZKWgPc6/F2T2S6MlbNlnG3Vit+rhh3swk=",
+                "delFlg": "POa4BMjxP1K9uqzyh5Sfbw40bL2g90IdSu66IniFxK4="
+            }
             ]
         }
     }
@@ -192,7 +192,7 @@ class Service {
 
     static getSearchHelp(key) {
         const task = (jsonDB) => {
-            return jsonDB[key]
+            return jsonDB[key] || {}
         }
         return this.fiddleDataSource(`./${this.lang}/$searchHelpDB.json`, task);
     }
